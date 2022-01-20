fb4ba43a74992fd3c9dc33d80397178e413b676d
MrGio7 <lashagiorgi1233@gmail.com>
2022-01-20 15:04:41 +0400

    added logout function

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import { useLogoutMutation } from "../generated/graphql";
 
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
+  const [logout, { client }] = useLogoutMutation();
+
   return (
     <>
       <header>
@@ -12,6 +15,15 @@ export const Home: React.FC<Props> = () => {
           <Link to="/register">Register</Link>
           <Link to="/login">Login</Link>
           <Link to="/bye">Bye</Link>
+          <button
+            onClick={async () => {
+              await logout();
+              localStorage.removeItem("accessToken");
+              await client.resetStore();
+            }}
+          >
+            logout
+          </button>
         </nav>
       </header>
       <Outlet />
